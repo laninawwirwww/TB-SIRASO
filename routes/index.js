2d736f613aa0ba78029dd65481130d63654f3558
IhsanurraisPardika <ihsanurrais06@gmail.com>
2025-06-17 20:27:23 +0700

    revisi 3

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,58 @@ router.get('/home', (req, res) => {
 
 // Arahkan root ke halaman login
 router.get('/', (req, res) => {
-  res.redirect('/login'); // Arahkan root ke halaman login
+  res.redirect('/users/login'); // Arahkan root ke halaman login
+});
+
+// post setelah register
+router.post('/register', (req, res) => {
+  const { username, password } = req.body;
+
+  // Cek apakah username sudah ada di database
+  prisma.user.findUnique({
+    where: { username: username }
+  })
+  .then(existingUser => {
+    if (existingUser) {
+      // Jika username sudah ada, beri pesan error
+      return res.render('register', { error: 'Username sudah digunakan' });
+    }
+
+    // Enkripsi password menggunakan bcrypt
+    bcrypt.hash(password, 10)
+      .then(hashedPassword => {
+        // Menyimpan pengguna baru di database
+        prisma.user.create({
+          data: {
+            username: username,
+            password: hashedPassword,  // Simpan password yang sudah terenkripsi
+            fullname: fullname,
+            phone: phone,
+            status: status,
+            address: address,
+            email: email,
+          },
+        })
+        .then(newUser => {
+          console.log('User created:', newUser);
+
+          // Setelah registrasi berhasil, arahkan ke halaman login
+          res.redirect('/users/login');
+        })
+        .catch(error => {
+          console.error('Error during user creation:', error);
+          res.status(500).send('Internal Server Error');
+        });
+      })
+      .catch(error => {
+        console.error('Error during password hashing:', error);
+        res.status(500).send('Internal Server Error');
+      });
+  })
+  .catch(error => {
+    console.error('Error during username check:', error);
+    res.status(500).send('Internal Server Error');
+  });
 });
 
 // rute untuk halaman register
